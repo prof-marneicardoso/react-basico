aafcfb63de6f86ce3957d3919410d01110a138ff
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2025-12-06 12:06:20 -0300

    Ajustes Estrutura

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+function App() {
+    return(
+        // Fragment <></>
+        <> 
+            
+            <h1>Olá, seja bem-vindo(a) ao meu site!</h1>
+        <h1>H1 Título 1</h1>
+        <h2>H2 Título 2</h2>
+        <h3>H3 Título 3</h3>
+        <h4>H4 Título 4</h4>
+        <h5>H5 Título 5</h5>
+        <h6>H6 Título 6</h6>
+
+        <p>
+            Consequuntur molestiae deserunt quo minima. <br />Nesciunt voluptatum dicta ab debitis expedita ipsum assumenda sint unde, vitae eaque neque, facere est tempora labore, soluta suscipit delectus illo beatae ipsa mollitia? Facilis quidem delectus inventore esse porro.
+        </p>
+
+        <p>
+    Tempore, expedita, minus placeat commodi perferendis, nesciunt obcaecati pariatur dolore incidunt ullam reiciendis dicta quia quis. Eum, magnam dicta reiciendis dolores quae excepturi deleniti eos, illo placeat suscipit labore id laborum tempora! Cupiditate quas obcaecati veniam. Harum nulla cupiditate esse repudiandae vitae sed voluptates incidunt unde, aliquid illo asperiores magnam, doloribus non pariatur tenetur ipsam quae perferendis!
+        </p>
+    
+        </>
+    );
+}
+
+// Highlander: "Só pode haver um!"
+export default App;
